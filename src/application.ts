2c5e8053d61f3dc4f265c74a7ebb0c564f296b62
shiu <wsh9075@163.com>
2021-06-30 00:15:30 +0800

    feat: implement interfaces

--- src/application.ts
+++ src/application.ts
@@ -1,28 +1,54 @@
-interface Vector{
+export interface VectorInterface{
   plus(vec:Vector):Vector
-  minus(vec:Vector):Vector
-  mulitply(vec:Vector):Vector
-  magnitude():number
+  dot(vec:Vector):number
+  normal():number
 
 }
-class Vector implements Vector{
+const dimError = new Error('vector plus operator need vectors have same length');
+
+export class Vector implements VectorInterface{
   private elements:number[];
-  constructor(elements:number[]){
-    this.elements = elements;
+  constructor(...args:number[]){
+    this.elements = args;
   }
   plus(vec: Vector) {
-    var el1 = vec.elements;
-    var el2 = this.elements;
-    if (el1.length !== el2.length) throw new Error('vector plus operator need vectors have same length')
-    return new Vector(el1.map(function (e, i) {
+    const el1 = vec.elements;
+    const el2 = this.elements;
+    if (el1.length !== el2.length) throw dimError;
+    return new Vector(...el1.map(function (e, i) {
       return e + el2[i]
     }));
   }
+
+  dot(vec:Vector):number{
+    const el1 = vec.elements;
+    const el2 = this.elements;
+    if(el1.length!==el2.length) throw dimError
+    return el1.map((e,i)=>e+el2[i]).reduce((sum,el)=>sum+el,0)
+  }
+  normal(){
+    const el = this.elements;
+    return Math.pow(el.reduce((sum,num)=>sum+Math.pow(num,2),0),0.5)
+  }
+
 }
-class Application {
-  private context:CanvasRenderingContext2D;
-  constructor(context:CanvasRenderingContext2D){
-    this.context = context;
+
+export class Application {
+  private context:CanvasRenderingContext2D|null;
+  private canvas:HTMLCanvasElement;
+  constructor(canvas:HTMLCanvasElement){
+    this.canvas = canvas;
+    this.context = this.canvas.getContext('2d');
+  }
+
+  public start(){
+
+  }
+  public stop(){
+
+  }
+  protected step(timeStamp:DOMHighResTimeStamp){
+    
   }
 
 }
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,4 @@
 console.log('hello world')
+import { Vector } from "./application"
+
+console.log(new Vector(1,2))
